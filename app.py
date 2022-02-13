1527c6560123fd764c30251b2bfdb5dc5abc79e5
Gloria-21 <gloriahn_82@hotmail.com>
2022-02-13 16:47:14 +0000

    Fix flash messages on base.hmtl

--- app.py
+++ app.py
@@ -142,4 +142,4 @@ def create(lot):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)
+            debug=True)
